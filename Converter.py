a54188e1287003bea320fd9c0d4156d156848027
22DP1AOstr <103511122+22DP1AOstr@users.noreply.github.com>
2023-03-21 19:02:30 +0200

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def convert_currency(amount, from_currency, to_currency):
+    api_key = "81b3b6afaff462547ea2c983" # Get your own API key from a currency exchange service provider
+    url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_currency}/{to_currency}/{amount}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    if data["result"] == "error":
+        return f"Error: {data['error-type']}"
+    else:
+        converted_amount = data["conversion_result"]
+        return f"{amount} {from_currency} is equal to {converted_amount} {to_currency}"
+
+amount = float(input("Enter the amount to convert: "))
+from_currency = input("Enter the currency to convert from: ")
+to_currency = input("Enter the currency to convert to: ")
+
+result = convert_currency(amount, from_currency, to_currency)
+print(result)
