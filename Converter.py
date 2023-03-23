b7c5f8870f3191eae4fe91de0e979a357ddb31d8
22DP1AOstr <103511122+22DP1AOstr@users.noreply.github.com>
2023-03-23 13:39:36 +0200

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -1,20 +1,33 @@
+from flask import Flask, render_templates
 import requests
 import json
 
-def convert_currency(amount, from_currency, to_currency):
-    api_key = "81b3b6afaff462547ea2c983" # My API key 
-    url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_currency}/{to_currency}/{amount}"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    if data["result"] == "error":
-        return f"Error: {data['error-type']}"
-    else:
-        converted_amount = data["conversion_result"]
-        return f"{amount} {from_currency} is equal to {converted_amount} {to_currency}"     # Atgriež konvertēto valūtu
+app = Flask(__name__)
 
-amount = float(input("Enter the amount to convert: "))
-from_currency = input("Enter the currency to convert from: ")
-to_currency = input("Enter the currency to convert to: ")
+@app.route("/", methods = ["POST", "GET"])
+def index():
 
-result = convert_currency(amount, from_currency, to_currency)
-print(result)
+    if requests.method == "POST":
+        def convert_currency(amount, from_currency, to_currency):
+            api_key = "81b3b6afaff462547ea2c983" # My API key 
+            url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_currency}/{to_currency}/{amount}"
+            response = requests.get(url)
+            data = json.loads(response.text)
+            if data["result"] == "error":
+                return f"Error: {data['error-type']}"
+            else:
+                converted_amount = data["conversion_result"]
+                return f"{amount} {from_currency} is equal to {converted_amount} {to_currency}"     # Atgriež konvertēto valūtu
+
+        amount = float(input("Enter the amount to convert: "))
+        from_currency = input("Enter the currency to convert from: ")
+        to_currency = input("Enter the currency to convert to: ")
+
+        result = convert_currency(amount, from_currency, to_currency)
+        print(result)
+    
+    else: 
+        return render_templates("Converter.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
